e47898b0fe8cf41e18fda83c30bc470d7a5e1fef
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-11 15:38:42 +0100

    completato header

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,38 @@
     <site_header/>
     <!-- /header -->
 
-    <site_main/>
+    <!-- <site_main/> -->
     <!-- / site main -->
 
-    <site_footer/>
+   <!--  <site_footer/> -->
     <!-- / footer -->
 
   </div>
 </template>
 
 <script>
+import site_header from './components/site_header.vue';
 
 export default {
   name: 'App',
   components: {
-   
+      site_header
   }
 }
 </script>
 
 <style lang="scss">
+  @import "./assets/css_var/colors.scss";
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: monospace;
+  }
+
+  #app {
+    height: 100vh;
+    background-color: $content_color;
+  }
 </style>
---
+++ src/components/site_header.vue
@@ -0,0 +1,98 @@
+<template>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="logoDc">
+            </div>
+            <!-- LOGO -->
+                <ul>
+                    <li>
+                        CHARACTERS  
+                    </li>
+                    <li class="active">
+                        COMICS
+                    </li>
+                    <li>
+                        MOVIES
+                    </li>
+                    <li>
+                        TV
+                    </li>
+                    <li>
+                        GAMES
+                    </li>
+                    <li>
+                        COLLECTIBLES
+                    </li>
+                    <li>
+                        VIDEOS
+                    </li>
+                    <li>
+                        FANS
+                    </li>
+                    <li>
+                        NEWS
+                    </li>
+                    <li>
+                        SHOP
+                    </li>
+                </ul>
+            <!-- /NAV LINKS -->
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "site_header"
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    @import '../assets/css_var/colors.scss';
+
+    header {
+        background-color: white;
+        
+        nav {
+            width: 80%;
+            margin: auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: stretch;
+            color: $content_color;
+            font-weight: bold;
+            background-color: white;
+            
+            
+            ul {
+                display: flex;
+                list-style-type: none;
+                line-height: calc(80px + 2rem);
+    
+                li {
+                    margin-left: 1rem;
+
+                    &:hover {
+                        color: $logo_color;
+                    }
+
+                    &.active {
+                        color: $logo_color;
+                        border-bottom: 3px solid $logo_color;
+                    }
+                }
+            }
+            
+            .logo {
+                width: 80px;
+                padding:  1rem 0;
+    
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+</style>
