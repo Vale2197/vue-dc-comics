68bd0016a466349db593526907449cb723c9bf14
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-12 11:50:19 +0100

    inserito componente card

---
+++ src/components/card.vue
@@ -0,0 +1,23 @@
+<template> 
+    <div class="card">
+        <img :src="image" alt="#">
+        <p>
+            {{description}}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "card",
+        props: {
+            image: String,
+            description: String,
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "../assets/css_var/colors.scss";
+
+</style>
